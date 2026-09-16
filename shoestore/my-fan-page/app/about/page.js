776ee07fe072843d88eb743d41f8a8822c73dd13
Andy Laique <andylaique@gmail.com>
2026-09-16 22:40:56 +0300

    Clean repo: remove unrelated shoestore files and pycache

--- shoestore/my-fan-page/app/about/page.js
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <main>
-      <h1>About this page</h1>
-      <p>Hi! I'm Ineza Teta, and this is my collection of favourites.</p>
-    </main>
-  );
-}
--- shoestore/my-fan-page/app/components/Hero.js
+++
@@ -1,8 +0,0 @@
-export default function Hero({ title, tagline }) {
-  return (
-    <header className="hero">
-      <h1>{title}</h1>
-      <p>{tagline}</p>
-    </header>
-  );
-}
